90a11a8886fb71078612e206f6c3c34116f06eeb
Amir Modarresi <amir.modarresi.13@gmail.com>
2018-06-15 16:36:06 +0430

    install composer in phpfpm image

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ cp docker/nginx/ssl/cert.pem.dist docker/nginx/ssl/cert.pem
 
 docker-compose up -d
 
-composer install --no-scripts
+docker-compose exec --user www-data phpfpm composer install
 
 sleep 10
 
