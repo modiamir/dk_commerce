4831f4c22ae6b0ba199b2191f238540f561fa852
Amir Modarresi <amir.modarresi.13@gmail.com>
2018-06-15 16:18:16 +0430

    hotifx in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp .env.dist .env
 docker-compose down
 docker-compose build
 
@@ -11,8 +12,6 @@ docker-compose up -d
 composer install --no-scripts
 
 sleep 10
-
-cp .env.dist .env
 
 docker-compose exec --user www-data phpfpm vendor/bin/doctrine orm:schema-tool:update --force
 docker-compose exec --user www-data phpfpm bin/digicli digikala:admin:create
